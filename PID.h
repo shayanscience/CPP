a5fb9c73870f800b2aafea3f541090a212c8dd90
Shayan Rezaei <47038661+shayanscience@users.noreply.github.com>
2021-04-09 10:39:20 -0700

    Update PID.h

--- PID.h
+++ PID.h
@@ -17,21 +17,21 @@
 class pid_control {
 private:
 	/* Declaring and Initializing some variables */
-	float	ki = 0.0,	// Integral Coefficient.
-		kp = 0.0,		// Proportional Coefficient.
-		kd = 0.0;		// Derivative Coefficient.
+	float	ki = 0.0f,	// Integral Coefficient.
+		kp = 0.0f,		// Proportional Coefficient.
+		kd = 0.0f;		// Derivative Coefficient.
 public:
 	/* Declaring and Initializing some variables */
-	float	P = 0.0,		// Error.
-		I = 0.0,		// Integral -> Error accumulated over time.
-		D = 0.0,		// Derivative -> Rate of change.
-		oldI = 0.0,		// Integral previous value.
-		oldP = 0.0,		// Error previous value.
-		oldD = 0.0,		// Derivative previous value.
-		oldT = 0.0,		// Previous time.
-		oldOutput = 0.0,// Previous output.
+	float	P = 0.0f,		// Error.
+		I = 0.0f,		// Integral -> Error accumulated over time.
+		D = 0.0f,		// Derivative -> Rate of change.
+		oldI = 0.0f,		// Integral previous value.
+		oldP = 0.0f,		// Error previous value.
+		oldD = 0.0f,		// Derivative previous value.
+		oldT = 0.0f,		// Previous time.
+		oldOutput = 0.0f,// Previous output.
 		newInput,		// Input of the system -> output of the pid_seek function.
-		dt = 0.0;		// Time Interval, dt = now - then.
+		dt = 0.0f;		// Time Interval, dt = now - then.
 
 	
 	void pid_init(float kp_coef, float ki_coef, float kd_coef);
